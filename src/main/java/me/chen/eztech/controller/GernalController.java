c16ab28d8b75e045ac1173623240de361a2b6f1e
lastcow <zhijiang@chen.me>
2019-05-24 00:35:55 -0400

    Project page.

--- src/main/java/me/chen/eztech/controller/GernalController.java
+++ src/main/java/me/chen/eztech/controller/GernalController.java
@@ -11,7 +11,7 @@ public class GernalController {
         return "login";
     }
 
-    @GetMapping(value = "/dashboard")
+    @GetMapping(value = "/admin/dashboard")
     public String dashboard(){
         return "dashboard";
     }
